d00158aa6a4d46620e44fa12a0bdf0dd40e60417
DaniloRibeiro <danilo_srr@hotmail.com>
2022-01-26 14:43:54 -0300

    button activate when condition mets

--- functions.js
+++ functions.js
@@ -1,5 +1,8 @@
 
 /* Prato principal */
+let prato
+let bebida
+let sobremesa
 
     function selecionado_frango(){
     
@@ -7,6 +10,16 @@
         document.getElementById("bife").style.borderStyle="hidden";
         document.getElementById("salmao").style.borderStyle="hidden";
       
+        prato = 'frango';
+
+        
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
     };
     function selecionado_bife(){
     
@@ -14,6 +27,16 @@
         document.getElementById("bife").style.borderStyle="solid";
         document.getElementById("salmao").style.borderStyle="hidden";
       
+         prato = 'bife';
+
+         
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
     };
     function selecionado_salmao(){
     
@@ -21,6 +44,16 @@
         document.getElementById("bife").style.borderStyle="hidden";
         document.getElementById("salmao").style.borderStyle="solid";
       
+         prato = 'salmao';
+
+         
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
     };
 
 /* Acompanhamento bebida*/
@@ -32,7 +65,19 @@ function selecionado_Coca(){
     document.getElementById("Sprite").style.borderStyle="hidden";
     document.getElementById("FantaLaranja").style.borderStyle="hidden";
     document.getElementById("FantaUva").style.borderStyle="hidden";
-  
+
+     bebida = 'coca';
+     check2 = 'ok'; 
+
+     
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+       
+    }
 };
 function selecionado_Refri(){
 
@@ -41,6 +86,19 @@ function selecionado_Refri(){
     document.getElementById("Sprite").style.borderStyle="hidden";
     document.getElementById("FantaLaranja").style.borderStyle="hidden";
     document.getElementById("FantaUva").style.borderStyle="hidden";
+
+     bebida = 'guarana';
+     check2 = 'ok'; 
+
+     
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+       
+    }
 };
 function selecionado_Sprite(){
 
@@ -49,7 +107,18 @@ function selecionado_Sprite(){
     document.getElementById("Sprite").style.borderStyle="solid";
     document.getElementById("FantaLaranja").style.borderStyle="hidden";
     document.getElementById("FantaUva").style.borderStyle="hidden";
-  
+
+     bebida = 'sprite';
+     check2 = 'ok'; 
+
+
+     if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
 };
 function selecionado_FantaLaranja(){
 
@@ -59,6 +128,16 @@ function selecionado_FantaLaranja(){
     document.getElementById("FantaLaranja").style.borderStyle="solid";
     document.getElementById("FantaUva").style.borderStyle="hidden";
   
+     bebida = 'fanta laranja';
+     check2 = 'ok'; 
+
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
 };
 
 function selecionado_FantaUva(){
@@ -69,6 +148,17 @@ function selecionado_FantaUva(){
     document.getElementById("FantaLaranja").style.borderStyle="hidden";
     document.getElementById("FantaUva").style.borderStyle="solid";
   
+     bebida = 'fanta uva';
+     check2 = 'ok'; 
+     
+
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+       
+    }
 };
 
 /* Acompanhamento sobremesa*/
@@ -78,19 +168,52 @@ function selecionado_Pudim(){
     document.getElementById("Pudim").style.borderStyle="solid";
     document.getElementById("Gelatina").style.borderStyle="hidden";
     document.getElementById("Mousse").style.borderStyle="hidden";
+
+     sobremesa = 'pudim';
+     check3 = 'ok'; 
   
+     
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+
+    }
 };
 function selecionado_Gelatina(){
 
     document.getElementById("Pudim").style.borderStyle="hidden";
     document.getElementById("Gelatina").style.borderStyle="solid";
     document.getElementById("Mousse").style.borderStyle="hidden";
-  
+
+     sobremesa = 'gelatina';
+     check3 = 'ok'; 
+
+     
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+
+    }
 };
 function selecionado_Mousse(){
 
     document.getElementById("Pudim").style.borderStyle="hidden";
     document.getElementById("Gelatina").style.borderStyle="hidden";
     document.getElementById("Mousse").style.borderStyle="solid";
-  
+
+     sobremesa = 'mousse';
+     check3 = 'ok'; 
+
+
+    if (prato != undefined && bebida != undefined && sobremesa !=undefined){
+        
+        document.getElementById('botao').style.background="green";
+        document.getElementById("botao").disabled = false;
+        document.getElementById('btntext').textContent = 'Fechar pedido'
+
+    }
 };
